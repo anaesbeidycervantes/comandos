91a02a25fa52a890a6c3bc7b2b39bc3934798773
anaesbeidycervantes <161491649+anaesbeidycervantes@users.noreply.github.com>
2024-04-25 18:47:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const OrdenText = document.getElementById('OrdenText');
-    const ws = new WebSocket('ws://localhost:8080');
-
-    function interpretarComando(comando) {
-        return comando; // Retorna el comando tal como se recibió
+    
+    let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    
+    if (SpeechRecognition) {
+        obtenerUltimaOrden();
+    } else {
+        console.error('Reconocimiento de voz no soportado.');
     }
 
-    ws.addEventListener('message', function(event) {
-        const comandoRecibido = event.data;
-        const OrdenText = document.getElementById('OrdenText');
-        
-        // Actualizar texto en el index.html
-        OrdenText.textContent = interpretarComando(comandoRecibido);
-    });
-
-    // Obtener y mostrar la última orden al cargar la página
     function obtenerUltimaOrden() {
         const url = "https://660219919d7276a75552a2c5.mockapi.io/registro";
 
@@ -43,7 +37,4 @@ document.addEventListener('DOMContentLoaded', function () {
             OrdenText.textContent = 'Error al obtener órdenes.';
         });
     }
-
-    // Obtener y mostrar la última orden al cargar la página
-    obtenerUltimaOrden();
 });
